21ae34f6bd094aed58c890555ad29334a9b9e0e6
José Gomes <j.da.silva8.gomes@gmail.com>
2018-11-12 20:40:45 +0000

    ucqueixa: registrar feito

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_11_08_225356) do
+
   create_table "problemas", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -33,4 +34,5 @@ ActiveRecord::Schema.define(version: 2018_11_08_225356) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
