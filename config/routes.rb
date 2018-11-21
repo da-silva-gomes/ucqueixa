004a8e71cd9c8b1f27825b163a0d42a9df974516
José Gomes <j.da.silva8.gomes@gmail.com>
2018-11-21 02:20:14 +0000

    ucqueixa: edit and create done. working on about page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   resources :problemas
   devise_for :users
 
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
   end
-  
-  root 'problemas#index'
-  get 'listaproblemas' => 'problemas#list'
+
+  root "problemas#index"
+  get "listaproblemas" => "problemas#list"
+  get "about" => "pages#about"
 end
