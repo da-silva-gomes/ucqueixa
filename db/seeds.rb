0161d507e7a45f9b95b664bb2a95a6ac4d3de978
José Gomes <j.da.silva8.gomes@gmail.com>
2018-11-24 17:34:56 +0000

    ucqueixa: added seed to fill all the map problems shown.

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,6 +45,52 @@ problema3 = Problema.create! do |p|
 end
 puts "Third problem created, continuing seeding..."
 
-puts "Were created 2 users and a set of 3 problems, so the first page could display information."
+# create problema 4
+problema3 = Problema.create! do |p|
+  p.title = "Falta de autocarros que liguem ao Polo II"
+  p.description = "Como estudante do Polo II que não possui meio de transporte próprio, tal como a maioria dos restantes estudantes universitários, sofro de um problema extremamente grave, a falta de meios que me ajudem a alcançar aquele que é o meu espaço de aprendizagem e estudo. Existem apenas 2 autocarros que façam percursos diretos que incluam Polo II no seu trajeto, apenas 2 autocarros que ofereçam a centenas e centenas de estudantes um meio acessivel de chegarem às suas aulas."
+  p.user_id = user.id
+end
+puts "Fourth problem created, continuing seeding..."
 
+# create problema 5
+problema3 = Problema.create! do |p|
+  p.title = "Passeios junto à residência Polo II"
+  p.description = "A residência do Polo II, depois da recepção dos vários atletas que participaram nos EUSA, as condições e os estragos que esta recebeu durante a sua ocupação degradaram imenso aquilo que é o local de vivência para centenas de alunos. Para além disso, a falta de limpeza e os recursos que recaíem sobre cada aluno."
+  p.user_id = user2.id
+end
+puts "Fifth problem created, continuing seeding..."
+
+# create problema 6
+problema3 = Problema.create! do |p|
+  p.title = "Degradação dos edifícios"
+  p.description = "Cada vez mais ao longo do tempo se denota a degradação dos edificios do polo II e igualmente das condições que estes possuem. Paredes destruidas, zonas não acessiveis, caminhos estragados, acessos dificultados pela falta de manutenção ou controlo. Tudo isto prejudica os alunos que passam pelo Polo II e prejudicam o sentimento e imagem da Universidade de Coimbra no futuro."
+  p.user_id = user2.id
+end
+puts "Sixth problem created, continuing seeding..."
+
+# create problema 7
+problema3 = Problema.create! do |p|
+  p.title = "Elevadores e acessos não fucionam"
+  p.description = "Durante todos os dias, centenas de pessoas entram e saiem do Departamento de Informática. Este tem vários acessos e formas de encontrar as várias salas e locais do mesmo, mas infelizmente, o controlo e manutenção dos vários elevadores e zonas de ligações entre os vários pisos é escassa, o que faz com que os mesmos sejam inutilizaveis e prejudiquem o estudante."
+  p.user_id = user.id
+end
+puts "Seventh problem created, continuing seeding..."
+
+# create problema 8
+problema3 = Problema.create! do |p|
+  p.title = "Caminhos de terra"
+  p.description = "Apesar do Polo II ser uma zona pequena e todos os departamentos serem relativamente perto uns dos outros, os caminhos e espaços que os unem sofrem degradação e maus tratos todos os dias. Havendo ainda caminhos em terra que liguem departamentos, algo que não é de todo a condição ideal para oferecer aqueles que são estudantes desde Polo, e aqueles que durante 3 ou mais anos vivem neste local."
+  p.user_id = user2.id
+end
+puts "Eighth problem created, continuing seeding..."
+
+# create problema 9
+problema3 = Problema.create! do |p|
+  p.title = "Destruição do Leslie"
+  p.description = "Causas naturais fizeram do espaço do Polo II um pouco pior para habitar. Estas condições não foram de culpa direta de toda a Universidade de Coimbra, mas passado 2 meses do acontecimento, pouco foi feito para voltar a recompor as condições à normalidade. Árvores caidas, ramos partidos e espalhados por todo o lado, lixo ainda espalhado pelas estradas, entre outros. Estes problemas acarratam ainda mais problemas, o descontentamente é um deles, a falta de condições para os estudantes é outro."
+  p.user_id = user.id
+end
+puts "Nineth problem created, continuing seeding..."
+puts "Were created 2 users and a set of 3 problems, so the first page could display information."
 puts "All setup, seeding finished."
